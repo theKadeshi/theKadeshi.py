4bdb8e1137a70f000e508345db924c97e6fdd3b4
Nick <ntorgov@gmail.com>
2017-06-07 16:20:23 +0300

    Database test fix

--- tests/database_test.py
+++ tests/database_test.py
@@ -4,5 +4,6 @@ import modules.database as dbase
 
 class TestDatabaseMethods(unittest.TestCase):
     def test_get_hash_signatures(self):
-        dbase.get_hash_signatures()
+        db = dbase.Database()
+        db.get_hash_signatures()
         self.assertTrue(True)
