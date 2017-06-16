f630fe9c6eeda94ed31b4fa6ac4f47b80ac30a83
Nick <ntorgov@gmail.com>
2017-06-16 21:44:52 +0300

    Fix code quality

--- tests/heuristic_test.py
+++ tests/heuristic_test.py
@@ -3,7 +3,16 @@ import modules.heuristic as heuristic
 
 
 class TestHeuristicMethods(unittest.TestCase):
+    """
+    Класс проверки эвристического алгоритма
+    """
+    
     def test_get_hash_signatures(self):
+        """
+        Пустая функция проверки. Задел на будущее
+        
+        :return:
+        """
         h = heuristic.Heuristic()
         result = h.sigmoid(1)
         print(result)
