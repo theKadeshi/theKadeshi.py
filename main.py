0dce9aa76f59396318922bf907829a3a11a2507c
Nick <ntorgov@gmail.com>
2017-06-16 21:24:48 +0300

    Параметры в строке

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+
 import os
 # import json, requests
 import hashlib
 import time
 import re
+import argparse
 # import modules.cms as cms
 import modules.database as dbase
 import modules.report as report
@@ -18,6 +21,12 @@ class TheKadeshi:
     Основной класс
     """
     
+    # Путь к корню сайта
+    site_folder = "./"
+    
+    # Использовать ли цвет в выводе
+    no_color = False
+    
     # Список зараженных файлов
     anamnesis_list = []
     
@@ -30,6 +39,10 @@ class TheKadeshi:
     # База сигнатур
     signatures_database = {'h': [], 'r': []}
     
+    def __init__(self, site_path="./", no_color=False):
+        self.site_folder = site_path
+        self.no_color = no_color
+    
     def get_files_list(self):
         """
         Функция получения списка файлов
@@ -37,14 +50,13 @@ class TheKadeshi:
         :return: Ничего, просто заполняет список файлов
         """
         
-        for root, dirs, files in os.walk('./'):
+        for root, dirs, files in os.walk(self.site_folder):
             for name in files:
                 if name.endswith(permitted_extensions):
                     file_path = os.path.join(root, name)
                     file_size = os.path.getsize(file_path)
                     self.total_files_size = self.total_files_size + file_size
                     self.files_list.append({'path': file_path, 'size': file_size})
-        pass
     
     def load_signatures(self):
         """
@@ -159,14 +171,20 @@ class TheKadeshi:
             if time_delta != 0:
                 scan_speed = total_scanned // time_delta // 1024
             scanner_counter = scanner_counter + 1
-            
+
             file_message = cls.C_GREEN + "Clean" + cls.C_DEFAULT
+            if self.no_color:
+                file_message = "Clean"
+                
             if is_file_error:
                 file_message = "Error"
             else:
                 if not is_file_clean:
                     file_message = cls.C_RED + "Infected" + cls.C_DEFAULT + ": " + cls.C_L_YELLOW + anamnesis_element[
                         'title'] + cls.C_DEFAULT
+                    
+                    if self.no_color:
+                        file_message = "Infected: " + anamnesis_element['title']
             
             print('[{0:.2f}% | {1!s}kB/s] {2!s} ({3!s})'.format(current_progress, scan_speed, file_item['path'],
                                                                 file_message, sep=" ", end="", flush=True))
@@ -216,8 +234,25 @@ class TheKadeshi:
         rpt.output()
 
 
-print("Ready")
-kdsh = TheKadeshi()
+if __name__ == "__main__":
+    print("Ready")
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.register("type", "bool", lambda v: v.lower() == "true")
+    parser.add_argument(
+        "site",
+        default="./",
+        help="Site folder"
+    )
+    parser.add_argument(
+        "-nc", "--no-color",
+        action='store_true',
+        help="Disable color output"
+    )
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0')
+
+args = parser.parse_args()
+
+kdsh = TheKadeshi(args.site, args.no_color)
 kdsh.get_files_list()
 print("Found", len(kdsh.files_list), "files, ~", kdsh.total_files_size, "bytes")
 
