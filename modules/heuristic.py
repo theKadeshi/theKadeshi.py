02f743b656134c5787dbedb9b964db6e904c6bdf
Nick <ntorgov@gmail.com>
2017-06-17 21:11:04 +0300

    Heuristic

--- modules/heuristic.py
+++ modules/heuristic.py
@@ -8,17 +8,34 @@ class Heuristic:
     
     consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "x", "v", "w", "z"]
     
-    def sigmoid(self, x):
-        """ Функция сигмоиды """
-        return 2 / (1 + math.exp(-x / 2))
+    forbidden_functions = ["eval", "assert", "base64_decode", "str_rot13", "mail",
+                           "move_uploaded_file", "is_uploaded_file", "script",
+                           "fopen", "curl_init", "document.write", "$GLOBAL",
+                           "passthru", "system", "exec", "header", "preg_replace",
+                           "fromCharCode", "$_COOKIE", "$_POST", "$_GET", "copy", "navigator",
+                           "$_REQUEST", "array_filter", "str_replace"]
     
-    def validate_file_name(self, file_name):
+    def sigmoid(self, x):
         """
-        Функция проверки имени файла на стремность
+        Modified sigmoid function
         
-        :param file_name: Имя файла
-        :return:
+        :param x: Some value
+        :type x: float
+        :return: Some value between 0 and 1
+        :rtype: float
         """
+        return 2 / (1 + math.exp(-x / 2)) - 1
+    
+    def validate_consonants_file_name(self, file_name: str):
+        """
+        Validate file name
+        
+        :param file_name: File name
+        :type file_name: str
+        :return:
+        :rtype: float
+        """
+        
         warning_level = 0
         consonant_counter = 0
         temporary_warning_level = 0
@@ -26,12 +43,31 @@ class Heuristic:
             if letter in self.consonants:
                 consonant_counter += 1
                 
-                if consonant_counter >= 3:
+                if consonant_counter > 3:
                     if consonant_counter > temporary_warning_level:
                         temporary_warning_level = consonant_counter
             else:
                 consonant_counter = 0
         
         warning_level = self.sigmoid(temporary_warning_level)
-        print(consonant_counter, temporary_warning_level, warning_level)
+        # print(consonant_counter, temporary_warning_level, warning_level)
         return warning_level
+    
+    def validate_forbidden_functions(self, file_content: str):
+        """
+        Validate file content for the forbidden functions
+        
+        :param file_content: File content
+        :type file_content: str
+        :return: Validation result
+        :rtype: bool
+        """
+        
+        result = False
+        
+        for func in self.forbidden_functions:
+            if file_content.find(func) != -1:
+                result = True
+                break
+                
+        return result
